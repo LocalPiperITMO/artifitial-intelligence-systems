1d1fa6d0aa0183ec70da427a7a1236a80131ed17
Local Piper <artsor53@gmail.com>
2024-09-27 04:18:53 +0300

    feat: added stock weapons and categories

--- lab1/tf2.pl
+++ lab1/tf2.pl
@@ -31,3 +31,42 @@ class_abilities(spy, [disguise, backstab]).
 has_ability(Class, Ability) :-
     class_abilities(Class, Abilities),
     member(Ability, Abilities).
+
+% weapons (stock)
+weapon(scattergun).
+weapon(pistol).
+weapon(bat).
+weapon(rocket_launcher).
+weapon(shotgun).
+weapon(shovel).
+weapon(flamethrower).
+weapon(fire_axe).
+weapon(grenade_launcher).
+weapon(stickybomb_launcher).
+weapon(bottle).
+weapon(minigun).
+weapon(fists).
+weapon(wrench).
+weapon(syringe_gun).
+weapon(medigun).
+weapon(bonesaw).
+weapon(sniper_rifle).
+weapon(smg).
+weapon(kukri).
+weapon(revolver).
+weapon(sapper).
+weapon(knife).
+weapon(invisibility_watch).
+
+% weapon categories
+category(primary, [scattergun, rocket_launcher, flamethrower, 
+                  grenade_launcher, minigun, shotgun, syringe_gun, 
+                  sniper_rifle, revolver]).
+category(secondary, [pistol, shotgun, stickybomb_launcher,
+                      medigun, smg, sapper]).
+category(melee, [bat, shovel, fire_axe, bottle, fists, wrench, bonesaw, kukri, knife]).
+category(misc, [invisibility_watch]).
+
+has_category(Weapon, Category) :-
+    category(Category, Weapons),
+    member(Weapon, Weapons).
