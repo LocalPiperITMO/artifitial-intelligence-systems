a197f6ded16c59799f39c8ddba35295387b46737
Local Piper <artsor53@gmail.com>
2024-09-27 04:07:06 +0300

    feat: added class abilities and rule

--- lab1/tf2.pl
+++ lab1/tf2.pl
@@ -9,6 +9,7 @@ class(medic).
 class(sniper).
 class(spy).
 
+% roles
 role(offence, [scout, soldier, pyro]).
 role(defence, [demoman, heavy, engineer]).
 role(support, [medic, sniper, spy]).
@@ -16,3 +17,17 @@ role(support, [medic, sniper, spy]).
 has_role(Class, Role) :-
     role(Role, Classes),
     member(Class, Classes).
+
+% class-provided abilities
+class_abilities(scout, [speed, double_jump]).
+class_abilities(soldier, [rocket_jump]).
+class_abilities(pyro, [fire_resistance]).
+class_abilities(heavy, [slow, big_hp]).
+class_abilities(engineer, [build_sentry, build_dispencer, build_teleport]).
+class_abilities(medic, [heal]).
+class_abilities(sniper, [headshot]).
+class_abilities(spy, [disguise, backstab]).
+
+has_ability(Class, Ability) :-
+    class_abilities(Class, Abilities),
+    member(Ability, Abilities).
